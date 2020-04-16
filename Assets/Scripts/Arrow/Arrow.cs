519f2debdd4d78f1bbd4f463d4f052417a5f060a
Alex Ma <alexmaruichen@hotmail.com>
2020-04-16 23:16:09 +0800

    Add Arrow and firing

---
+++ Assets/Scripts/Arrow/Arrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    public int speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the vertical speed to make the bullet move upward
+    }
+
+    // Gets called when the object goes out of the screen
+    void OnBecameInvisible()
+    {
+        // Destroy the bullet
+        Destroy(gameObject);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Main character/MC.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MC : MonoBehaviour
+{
+    public GameObject arrow;
+    private Rigidbody2D rb;
+    private Vector2 v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        v = rb.velocity;
+        v.x = Input.GetAxis("Horizontal") * 2;
+        v.y = Input.GetAxis("Vertical") * 2;
+        rb.velocity = v;
+        Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = 360 - Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, 0, angle);
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(arrow, transform.position, transform.rotation);
+        }
+    }
+}
