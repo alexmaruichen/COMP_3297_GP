a776bd0cc931aef2d17ee7fa81cd8ef8c6e7cc79
h3553493 <47234665+h3553493@users.noreply.github.com>
2020-05-17 02:27:12 +0800

    Add files via upload

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public int method;
+    public AudioSource AS;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AS = GetComponent<AudioSource>();
     }
     void OnTriggerEnter2D(Collider2D obj)
     {
         string name = obj.gameObject.name;
         if (name == "Main")
+        {
+            AudioSource.PlayClipAtPoint(AS.clip, transform.position);
             Destroy(gameObject);
+        }
+            
     }
     // Update is called once per frame
     void Update()
